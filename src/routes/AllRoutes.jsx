c11e2f1bbdfca668576662f55d1d34653ba60cb8
bhnibir <bh.nibir@gmail.com>
2022-02-13 12:43:30 +0600

    update dev settings

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,47 +1,47 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AboutPage from '../pages/AboutPage';
-import AddTreatmentPage from '../pages/AddTreatmentPage';
-import ForgetPasswordPage from '../pages/ForgetPasswordPage';
-import HomePage from '../pages/HomePage/HomePage';
-import LoginPage from '../pages/LoginPage/LoginPage';
-import Page404 from '../pages/Page404';
-import ResetPasswordPage from '../pages/ResetPasswordPage';
-import SearchResultPage from '../pages/SearchResultPage';
-import SignUpPage from '../pages/SignUpPage/SignUpPage';
-import VerifyAccountPage from '../pages/VerifyAccountPage/VerifyAccountPage';
-import ProtectedRoute from './ProtectedRoute';
-import AddDiseasePage from '../pages/AddDiseasePage/AddDiseasePage';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "../pages/AboutPage";
+import AddDiseasePage from "../pages/AddDiseasePage/AddDiseasePage";
+import AddTreatmentPage from "../pages/AddTreatmentPage";
+import ForgetPasswordPage from "../pages/ForgetPasswordPage";
+import HomePage from "../pages/HomePage/HomePage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import Page404 from "../pages/Page404";
+import ResetPasswordPage from "../pages/ResetPasswordPage";
+import SearchResultPage from "../pages/SearchResultPage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage";
+import VerifyAccountPage from "../pages/VerifyAccountPage/VerifyAccountPage";
+import ProtectedRoute from "./ProtectedRoute";
 
 const AllRoutes = () => (
-    <Routes>
-        <Route
-            path="/"
-            element={
-                <ProtectedRoute auth>
-                    <HomePage />
-                </ProtectedRoute>
-            }
-        />
-        <Route
-            path="/home"
-            element={
-                <ProtectedRoute auth>
-                    <HomePage />
-                </ProtectedRoute>
-            }
-        />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="add-disease" element={<AddDiseasePage />} />
-        <Route path="add-treatment" element={<AddTreatmentPage />} />
-        <Route path="search" element={<SearchResultPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="sign-up" element={<SignUpPage />} />
-        <Route path="/activate/:token" element={<VerifyAccountPage />} />
-        <Route path="reset-password" element={<ResetPasswordPage />} />
-        <Route path="forget-password" element={<ForgetPasswordPage />} />
-        <Route path="/*" element={<Page404 />} />
-    </Routes>
+  <Routes>
+    <Route
+      path="/"
+      element={
+        <ProtectedRoute auth>
+          <HomePage />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/home"
+      element={
+        <ProtectedRoute auth>
+          <HomePage />
+        </ProtectedRoute>
+      }
+    />
+    <Route path="about" element={<AboutPage />} />
+    <Route path="add-disease" element={<AddDiseasePage />} />
+    <Route path="add-treatment" element={<AddTreatmentPage />} />
+    <Route path="search" element={<SearchResultPage />} />
+    <Route path="login" element={<LoginPage />} />
+    <Route path="sign-up" element={<SignUpPage />} />
+    <Route path="/activate/:token" element={<VerifyAccountPage />} />
+    <Route path="reset-password" element={<ResetPasswordPage />} />
+    <Route path="forget-password" element={<ForgetPasswordPage />} />
+    <Route path="/*" element={<Page404 />} />
+  </Routes>
 );
 
 export default AllRoutes;
--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-    const location = useLocation();
-    const authLogin = localStorage.getItem('token');
+  const location = useLocation();
+  const authLogin = localStorage.getItem("token");
 
-    if (!authLogin) {
-        return <Navigate to="/login" state={{ from: location }} />;
-    }
-    return children;
+  if (!authLogin) {
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+  return children;
 };
 
 export default ProtectedRoute;
